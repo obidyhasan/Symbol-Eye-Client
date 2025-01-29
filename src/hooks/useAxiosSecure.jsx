c09228c8b7ff7204c3c472aa4bb3c38fce46bb95
obidyhasan <obidyhasan@gmail.com>
2025-01-29 15:57:53 +0600

    include axiosSecure hook

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+const useAxiosSecure = () => {
+  const { handelFirebaseLogout } = useAuth();
+  const navigate = useNavigate();
+
+  // add a request interceptor
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  // add a response interceptor
+  axiosSecure.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        await handelFirebaseLogout();
+        navigate("/login");
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
