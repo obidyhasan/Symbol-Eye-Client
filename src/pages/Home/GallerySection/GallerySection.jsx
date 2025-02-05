b89a8c85d7e431371964df59f326a93144d1ad88
obidyhasan <obidyhasan@gmail.com>
2025-02-06 00:57:03 +0600

    include Gallery Section on Home Page

--- src/pages/Home/GallerySection/GallerySection.jsx
+++ src/pages/Home/GallerySection/GallerySection.jsx
@@ -1,6 +1,9 @@
 import Headline from "../../../components/Headline";
+import useGallery from "../../../hooks/useGallery";
 
 const GallerySection = () => {
+  const { gallery, isPending } = useGallery();
+
   return (
     <div className="max-width pb-10 pt-10">
       <Headline
@@ -9,33 +12,35 @@ const GallerySection = () => {
           "Explore our curated gallery to see how Symbol Eye brings style to life through every collection."
         }
       ></Headline>
+
       <div className="mt-10">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          <div>
-            <img src="" className="w-full h-[300px]" alt="" />
+        {isPending ? (
+          <div className="flex items-center justify-center py-10">
+            <span className="loading loading-spinner loading-lg"></span>
           </div>
+        ) : (
           <div>
-            <img src="" className="w-full h-[300px]" alt="" />
+            {gallery.length ? (
+              <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {gallery.map((img) => (
+                  <div key={img?._id}>
+                    <img
+                      src={img?.image}
+                      className="w-full h-[300px] bg-base-300"
+                      alt=""
+                    />
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div>
+                <p className="p-5 rounded text-center font-medium border border-base-200">
+                  Images Not Found
+                </p>
+              </div>
+            )}
           </div>
-          <div>
-            <img src="" className="w-full h-[300px]" alt="" />
-          </div>
-          <div>
-            <img src="" className="w-full h-[300px]" alt="" />
-          </div>
-          <div>
-            <img src="" className="w-full h-[300px]" alt="" />
-          </div>
-          <div>
-            <img src="" className="w-full h-[300px]" alt="" />
-          </div>
-          <div>
-            <img src="" className="w-full h-[300px]" alt="" />
-          </div>
-          <div>
-            <img src="" className="w-full h-[300px]" alt="" />
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
